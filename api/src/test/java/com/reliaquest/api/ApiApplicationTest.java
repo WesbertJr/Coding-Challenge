d8d83d1e494d9af74dd8e2de82cd2fca5d662925
WesbertJr <wesbertjr@gmail.com>
2025-03-03 15:19:39 -0500

    updated build.gradle and additional config changes

--- api/src/test/java/com/reliaquest/api/ApiApplicationTest.java
+++ api/src/test/java/com/reliaquest/api/ApiApplicationTest.java
@@ -1,13 +1,20 @@
 package com.reliaquest.api;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApiApplicationTest {
 
+    @LocalServerPort
+    private int port;
+
     @Test
-    void someTest() {
-        // do the thing here
+    void applicationStartsSuccessfully() {
+        assertThat(port).isGreaterThan(0);
     }
 }
